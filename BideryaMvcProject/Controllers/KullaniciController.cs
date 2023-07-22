760cf31062dee0ddd73a0ff94c2a262245e75d8f
Servet <sena zabun@DESKTOP-7FIIBG4>
2023-07-22 17:43:40 +0300

    pusladım

---
+++ BideryaMvcProject/Controllers/KullaniciController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BideryaMvcProject.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GirisYap()
+        {
+            return View();
+        }
+        public IActionResult HesapOlustur()
+        {
+            return View();
+        }
+        
+        
+    }
+}
