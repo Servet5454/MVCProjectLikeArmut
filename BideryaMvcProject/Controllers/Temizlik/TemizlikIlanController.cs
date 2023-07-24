109715e4b5393f275a5ebee2a251b1986d1f7f50
Servet <sena zabun@DESKTOP-7FIIBG4>
2023-07-24 17:36:21 +0300

    anasayfa düzenlendi koltuk hizmet sayfası eklendi

---
+++ BideryaMvcProject/Controllers/Temizlik/TemizlikIlanController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BideryaMvcProject.Controllers.Temizlik
+{
+    public class TemizlikIlanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult KoltukTemizlik()
+        {
+            return View();
+        }
+    }
+}
