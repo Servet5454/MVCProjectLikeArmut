81ccb081155f77dd7eb9bc6e7389155c315f4fcf
Servet <sena zabun@DESKTOP-7FIIBG4>
2023-08-01 14:30:32 +0300

    tadilatController Yapıldı

---
+++ BideryaMvcProject/Controllers/TadilatVeDekorasyon/TadilatVeDekorasyonController.cs
@@ -0,0 +1,38 @@
+﻿using BideryaMvcProject.DataBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BideryaMvcProject.Controllers.TadilatVeDekorasyon
+{
+    public class TadilatVeDekorasyonController : Controller
+    {
+        private readonly BideryaMvcDatabase? context;
+        private readonly IConfiguration configuration;
+
+
+        public TadilatVeDekorasyonController(BideryaMvcDatabase? context, IConfiguration configuration)
+        {
+            this.context = context;
+            this.configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CamBalkon()
+        {
+            return View();
+        }
+        public IActionResult EvTadilat()
+        {
+            return View();
+        }
+        public IActionResult Mantolama()
+        {
+            return View();
+        }
+        public IActionResult MutfakDolabiYapimi()
+        {
+            return View();
+        }
+    }
+}
