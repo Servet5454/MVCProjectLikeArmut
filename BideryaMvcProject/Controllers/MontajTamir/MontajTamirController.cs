6803a51efd32097905f9db30891d14b82ebfb706
Servet <sena zabun@DESKTOP-7FIIBG4>
2023-08-01 16:52:29 +0300

    Layout Navigation Düzenlendi Bitek Organizasyon Kaldı

---
+++ BideryaMvcProject/Controllers/MontajTamir/MontajTamirController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BideryaMvcProject.Controllers.MontajTamir
+{
+    public class MontajTamirController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult KombiTamiri()
+        {
+            return View();
+        }
+        public IActionResult MobilyaSokmeMontaj()
+        {
+            return View();
+        }
+        public IActionResult KlimaMontaj()
+        {
+            return View();
+        }
+        public IActionResult SuKacakTespiti()
+        {
+            return View();
+        }
+        public IActionResult AsansorBakimi()
+        {
+            return View();
+        }
+    }
+}
---
+++ BideryaMvcProject/Controllers/Nakliyat/NakliyatController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BideryaMvcProject.Controllers.Nakliyat
+{
+    public class NakliyatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EvdenEveNakliyat()
+        {
+            return View();
+        }
+        public IActionResult UluslararasiNakliyat()
+        {
+            return View();
+        }
+        public IActionResult SehirlerArasiNakliyat()
+        {
+            return View();
+        }
+        public IActionResult AsansorluNakliyat()
+        {
+            return View();
+        }
+        public IActionResult DepoKiralama()
+        {
+            return View();
+        }
+    }
+}
