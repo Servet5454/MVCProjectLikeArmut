d422d1bff02616a092c38c093be7c33c64901bf3
Servet <sena zabun@DESKTOP-7FIIBG4>
2023-08-05 15:59:40 +0300

    koltukta yapıldı

--- BideryaMvcProject/Models/Ilanlar/Temizlik/KoltukTemizlikViewModel.cs
+++ BideryaMvcProject/Models/Ilanlar/Temizlik/KoltukTemizlikViewModel.cs
@@ -4,11 +4,11 @@ namespace BideryaMvcProject.Models.Ilanlar.Temizlik
 {
     public class KoltukTemizlikViewModel : IlanAdresGenelBilgi
     {
-        public int Id { get; set; }
+        
         public int TekliKoltukSayisi { get; set; }
         public int IkiliKoltukSayisi { get; set; }
         public int UcluKoltukSayisi { get; set; }
-        public int LKoltukSayisi { get; set; }
+       
         public int SandalyeSayisi { get; set; }
         public int MinderSayisi { get; set; }
         public int TekliYatakSayisi { get; set; }
