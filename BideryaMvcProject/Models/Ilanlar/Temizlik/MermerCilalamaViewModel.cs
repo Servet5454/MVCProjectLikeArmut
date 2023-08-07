ddef09c2757a06ca2353bc48f97a770ca62a2203
Servet <sena zabun@DESKTOP-7FIIBG4>
2023-08-07 10:32:15 +0300

    Cilalama Düzenlendi

--- BideryaMvcProject/Models/Ilanlar/Temizlik/MermerCilalamaViewModel.cs
+++ BideryaMvcProject/Models/Ilanlar/Temizlik/MermerCilalamaViewModel.cs
@@ -9,6 +9,8 @@ namespace BideryaMvcProject.Models.Ilanlar.Temizlik
         public int IlanAltKategoriId { get; set; } = Convert.ToInt32(AltKategoriEnum.TemizlikAltKategori.MermerCilalama);
         public bool Aktifmi { get; set; } = false;
         public int TeklifSayisi { get; set; } = 0;
+        public string? Il { get; set; }
+        public string? Ilce { get; set; }
         public string? IlanBaslik { get; set; } = "Mermer Cilalama";
         #endregion
         public string? MekanTipi { get; set; }
